f9d280a6cd512e92696d192c796ed69d4d2df9e0
Cesar Rafael Lopes <ceslop84@gmail.com>
2021-04-23 08:08:01 -0300

    Salvamento...

---
+++ env.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/mpeg7/linux/solibs
+chmod +x $PWD/mpeg7/linux/MPEG7Fex
--- mpeg.py
+++ mpeg.py
@@ -31,18 +31,20 @@ with open(file_tmp, "w") as f:
     for obj in f_list:
         f.write(f"{obj}\n")
 
+if os.name == "posix":
+        subprocess.run("./env.sh", shell=True)
+
 for d in desc_list:
     if os.name == "nt":
         subprocess.run(f"mpeg7/windows/MPEG7Fex.exe {d} {file_tmp} {mpeg_folder}/{d}.txt", shell=True)
     elif os.name == "posix":
-        subprocess.run("sh env.sh", shell=True)
-        subprocess.run(f"./mpeg7/linux/MPEG7Fex CSD 64 {d} {file_tmp} {mpeg_folder}/{d}.txt", shell=True)
+        subprocess.run(f"./mpeg7/linux/MPEG7Fex {d} {file_tmp} {mpeg_folder}/{d}.txt", shell=True)
     else:
         print("Sorry, OS not supported!")
         exit(1)
 
 # Removing the temp file.
-os.remove(img_list_out)
+os.remove(file_tmp)
 
 
 
